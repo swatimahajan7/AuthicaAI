8dcdea7f9188502566a62b5356124024e09fd6c9
Swati Mahajan <swati.mahajan@acldigital.com>
2025-08-06 16:11:34 +0530

    Alerts Page and Navbar adjustments for the same

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,136 +15,29 @@ import MenuItem from "@mui/material/MenuItem";
 import Badge from "@mui/material/Badge";
 import { NotificationsList, type NotificationItem } from "./NotificationsList";
 import { useNavigate } from "react-router";
-import { Dialog, DialogTitle, DialogContent, DialogContentText } from "@mui/material";
 
 type ResponsiveAppBarProps = {
+  notifications: NotificationItem[];
   onTabChange?: (tabName: string) => void;
+  onNotificationSelect?: (notif: NotificationItem) => void;
 };
 
-const pages = ["Users", "Risk Policy"];
+const pages = ["Users", "Risk Policy", "Alerts"];
 const settings = ["Logout"];
 
-const initialNotifications: NotificationItem[] = [
-  {
-    id: 1,
-    message: "Mike Chen has logged in from a different location than usual",
-    read: false,
-    title: "Login from New Location Detected",
-    details: {
-      device: "MacBook Pro (Safari 17.1)",
-      location: {
-        city: "San Francisco",
-        country: "USA",
-        coordinates: { lat: 37.7749, lon: -122.4194 }
-      },
-      ip: "192.168.45.23",
-      loginTime: "2025-08-01T09:23:15Z"
-    }
-  },
-  {
-    id: 2,
-    message: "John Doe has logged in at an unusual time",
-    read: false,
-    title: "Login at Unusual Time Detected",
-    details: {
-      device: "Windows 11 PC (Chrome 126)",
-      location: {
-        city: "New York",
-        country: "USA",
-        coordinates: { lat: 40.7128, lon: -74.0060 }
-      },
-      ip: "10.14.67.89",
-      loginTime: "2025-08-01T03:45:02Z"
-    }
-  },
-  {
-    id: 3,
-    message: "David Kim logged in at an unusual time",
-    read: false,
-    title: "Unusual Login Activity",
-    details: {
-      device: "Samsung Galaxy S23 (Android, Chrome 125)",
-      location: {
-        city: "Seoul",
-        country: "South Korea",
-        coordinates: { lat: 37.5665, lon: 126.9780 }
-      },
-      ip: "172.16.23.54",
-      loginTime: "2025-08-01T04:12:39Z"
-    }
-  },
-  {
-    id: 4,
-    message: "David Kim logged in at an unusual time",
-    read: true,
-    title: "Unusual Login Activity",
-    details: {
-      device: "iPad Pro (Safari 17.0)",
-      location: {
-        city: "Busan",
-        country: "South Korea",
-        coordinates: { lat: 35.1796, lon: 129.0756 }
-      },
-      ip: "172.16.45.77",
-      loginTime: "2025-07-31T23:56:27Z"
-    }
-  },
-  {
-    id: 5,
-    message: "Emily Rodriguez has logged in from a different location than usual",
-    read: true,
-    title: "Login from New Location Detected",
-    details: {
-      device: "MacBook Air (Chrome 126)",
-      location: {
-        city: "Toronto",
-        country: "Canada",
-        coordinates: { lat: 43.65107, lon: -79.347015 }
-      },
-      ip: "10.24.98.102",
-      loginTime: "2025-07-31T15:33:44Z"
-    }
-  }
-];
-
-
-function ResponsiveAppBar({ onTabChange }: ResponsiveAppBarProps) {
+function ResponsiveAppBar({ notifications, onTabChange, onNotificationSelect }: ResponsiveAppBarProps) {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
   const [anchorElNotifications, setAnchorElNotifications] = React.useState<null | HTMLElement>(null);
-  const [notifications, setNotifications] = React.useState<NotificationItem[]>(initialNotifications);
-  const [selectedNotification, setSelectedNotification] = React.useState<NotificationItem>({} as NotificationItem)
-  const [openNotificationPopup, setOpenNotificationPopup] = React.useState<boolean>(false)
 
   const navigate = useNavigate();
-
   const unreadCount = notifications.filter((n) => !n.read).length;
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleOpenNotificationsMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNotifications(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => setAnchorElNav(null);
-  const handleCloseUserMenu = () => setAnchorElUser(null);
-  const handleCloseNotificationsMenu = () => setAnchorElNotifications(null);
-
   const handleNotificationClick = (id: number) => {
-    setNotifications((prev) =>
-      prev.map((n) =>
-        n.id === id ? { ...n, read: true } : n
-      )
-    );
-    setSelectedNotification(notifications.find((n) => n.id === id)!)
-    setOpenNotificationPopup(true)
-    handleCloseNotificationsMenu();
+    const notif = notifications.find((n) => n.id === id)!;
+    onTabChange?.("Alerts");
+    onNotificationSelect?.(notif);
+    setAnchorElNotifications(null);
   };
 
   return (
@@ -163,37 +56,25 @@ function ResponsiveAppBar({ onTabChange }: ResponsiveAppBarProps) {
                 color: "#3b354bff",
                 letterSpacing: "1px",
                 fontSize: { xs: "1.25rem", md: "2rem" },
-
               }}
             >
               AuthicaAI
             </Typography>
 
             <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="menu"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
+              <IconButton size="large" onClick={(e) => setAnchorElNav(e.currentTarget)} color="inherit">
                 <MenuIcon />
               </IconButton>
               <Menu
-                id="menu-appbar"
                 anchorEl={anchorElNav}
-                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-                transformOrigin={{ vertical: "top", horizontal: "left" }}
                 open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{ display: { xs: "block", md: "none" } }}
+                onClose={() => setAnchorElNav(null)}
               >
                 {pages.map((page) => (
                   <MenuItem
                     key={page}
                     onClick={() => {
-                      handleCloseNavMenu();
+                      setAnchorElNav(null);
                       onTabChange?.(page);
                     }}
                   >
@@ -203,38 +84,18 @@ function ResponsiveAppBar({ onTabChange }: ResponsiveAppBarProps) {
               </Menu>
             </Box>
 
-            <Typography
-              variant="h5"
-              noWrap
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "Lato",
-                fontWeight: 700,
-                color: "inherit",
-              }}
-            >
-              AuthicaAI
-            </Typography>
-
             <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
               {pages.map((page) => (
                 <Button
                   key={page}
-                  onClick={() => {
-                    handleCloseNavMenu();
-                    onTabChange?.(page);
-                  }}
+                  onClick={() => onTabChange?.(page)}
                   sx={{
                     my: 2,
                     color: "white",
                     display: "block",
-                    "&:hover": {
-                      backgroundColor: "primary.dark",
-                    },
-                    textTransform: 'none',
-                    fontSize: 16
+                    "&:hover": { backgroundColor: "primary.dark" },
+                    textTransform: "none",
+                    fontSize: 16,
                   }}
                 >
                   {page}
@@ -244,49 +105,34 @@ function ResponsiveAppBar({ onTabChange }: ResponsiveAppBarProps) {
 
             <Box sx={{ flexGrow: 0, display: "flex", alignItems: "center" }}>
               <Tooltip title="View notifications">
-                <IconButton
-                  onClick={handleOpenNotificationsMenu}
-                  sx={{ p: 0, mr: 2, color: "inherit" }}
-                >
-                  <Badge
-                    color="error"
-                    variant={unreadCount > 0 ? "dot" : "standard"}
-                    overlap="circular"
-                  >
-                    <NotificationsIcon />
+                <IconButton onClick={(e) => setAnchorElNotifications(e.currentTarget)} sx={{ p: 0, mr: 2 }}>
+                  <Badge color="error" variant={unreadCount > 0 ? "dot" : "standard"} overlap="circular">
+                    <NotificationsIcon sx={{
+                      color: 'white'
+                    }} />
                   </Badge>
                 </IconButton>
               </Tooltip>
               <Menu
                 anchorEl={anchorElNotifications}
                 open={Boolean(anchorElNotifications)}
-                onClose={handleCloseNotificationsMenu}
-                anchorOrigin={{ vertical: "top", horizontal: "right" }}
-                transformOrigin={{ vertical: "top", horizontal: "right" }}
-                sx={{ mt: "45px" }}
+                onClose={() => setAnchorElNotifications(null)}
               >
-                <NotificationsList
-                  notifications={notifications}
-                  onItemClick={(id) => handleNotificationClick(id)}
-                />
+                <NotificationsList notifications={notifications} onItemClick={handleNotificationClick} />
               </Menu>
 
               <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <IconButton onClick={(e) => setAnchorElUser(e.currentTarget)} sx={{ p: 0 }}>
                   <Avatar alt="Admin Avatar" src="/static/images/avatar/2.jpg" />
                 </IconButton>
               </Tooltip>
               <Menu
-                id="menu-appbar"
                 anchorEl={anchorElUser}
-                anchorOrigin={{ vertical: "top", horizontal: "right" }}
-                transformOrigin={{ vertical: "top", horizontal: "right" }}
                 open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-                sx={{ mt: "45px" }}
+                onClose={() => setAnchorElUser(null)}
               >
                 {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={() => navigate('/signin')}>
+                  <MenuItem key={setting} onClick={() => navigate("/signin")}>
                     <Typography textAlign="center">{setting}</Typography>
                   </MenuItem>
                 ))}
@@ -295,32 +141,8 @@ function ResponsiveAppBar({ onTabChange }: ResponsiveAppBarProps) {
           </Toolbar>
         </Container>
       </AppBar>
-      {
-        openNotificationPopup && <Dialog
-          open={openNotificationPopup}
-          onClose={() => setOpenNotificationPopup(false)}
-        >
-          <DialogTitle>
-            {selectedNotification.title}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText component="div">
-              <p><strong>Device:</strong> {selectedNotification.details.device}</p>
-              <p><strong>Location:</strong> {selectedNotification.details.location.city}, {selectedNotification.details.location.country}</p>
-              <p>
-                <strong>Coordinates:</strong>
-                {selectedNotification.details.location.coordinates.lat},
-                {selectedNotification.details.location.coordinates.lon}
-              </p>
-              <p><strong>IP Address:</strong> {selectedNotification.details.ip}</p>
-              <p><strong>Login Time:</strong> {new Date(selectedNotification.details.loginTime).toLocaleString()}</p>
-            </DialogContentText>
-          </DialogContent>
-        </Dialog>
-      }
     </>
   );
 }
 
 export default ResponsiveAppBar;
-
